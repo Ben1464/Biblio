a42cd4ff3b55c09aabea333c4c5d87f553a4bc00
Ben <kimaniben14@gmail.com>
2024-05-29 23:23:18 +0100

    updates on styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Bibliotech</h1>
+        <h1 className='app-header'>Welcome to Bibliotech</h1>
       </header>
       <main>
         <Booklist />
