0603aabc2502961235af3a3c1c331e6dda869124
Ben <kimaniben14@gmail.com>
2024-05-29 18:15:31 +0100

    initial commit

---
+++ src/components/Booklist.js
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import '../styles/Booklist.css';
+import AtomicImage from '../assets/images/atomic.jpg'; // Import the image
+import LawsImage from'../assets/images/48Laws.webp';
+import AlphaImage from '../assets/images/unplugged alpha.jpg';
+import ChemImage from '../assets/images/The ALchemist.jpg';
+import AirImage from '../assets/images/when breathbecomes.jpg';
+import LeanImage from '../assets/images/Lean start.jpg';
+import HomoImage from '../assets/images/Homo dues.jpg';
+import SemenImage from '../assets/images/Semen.jpg';
+import CantImage from '../assets/images/Cant hurt .jpg';
+const books = [
+  {
+    picture: AtomicImage, // Use the imported image variable
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    genre: "Personal Help",
+    availability: true,
+    Download: '../assets/pdfs/Atomic-Habits-.pdf'
+  },
+  {
+    picture: LawsImage, // Use the imported image variable
+    title: '48 Laws Of Power',
+    author: 'Robert Greene',
+    genre: "Personal Help",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: AlphaImage, // Use the imported image variable
+    title: 'Unplugged Alpha',
+    author: 'Richard Copper',
+    genre: "Masculinity",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: ChemImage, // Use the imported image variable
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+    genre: "Fantasy/Adventure",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: AirImage, // Use the imported image variable
+    title: 'When Breath Becomes Air',
+    author: 'Paul Kalanithi',
+    genre: "Autobiography",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: LawsImage, // Use the imported image variable
+    title: 'Mastery',
+    author: 'Robert Greene',
+    genre: "Personal Help",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: LeanImage, // Use the imported image variable
+    title: 'Lean Startup',
+    author: 'Eric Ries',
+    genre: "Business",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: HomoImage, // Use the imported image variable
+    title: 'Homo Deus',
+    author: 'Yuval Noah Harari',
+    genre: "Science/Philosophy",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+
+{
+    picture:SemenImage, // Use the imported image variable
+    title: 'Semen Retention',
+    author: 'Joseph Peterson',
+    genre: "Masculinity/Health",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+  {
+    picture: CantImage, // Use the imported image variable
+    title: 'Cant Hurt Me',
+    author: 'David Goggins',
+    genre: "Personal Help",
+    availability: true,
+    Download: 'https://www.amazon.com/Atomic-Habits-Nick-Vujic/dp/0735211299'
+  },
+];
+
+function Booklist() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredBooks = books.filter(book =>
+    book.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    book.author.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search by title or author"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="search-input"
+        />
+      </div>
+      <div>
+        <ol className="book-list">
+          {filteredBooks.map((book, index) => (
+            <li key={index} className="book-item">
+              <img src={book.picture} alt={book.title} className="book-image" />
+              <div className="book-info">
+                <h3 className="book-title">{book.title}</h3>
+                <p className="book-author">{book.author}</p>
+                <h4 className='genre'>Genre</h4>
+                <p className='book-genre'>{book.genre}</p>
+                <p className={`book-availability ${book.availability ? 'available' : 'unavailable'}`}>
+                  {book.availability ? 'Available' : 'Unavailable'}
+                </p>
+                <a href={book.link} target="_blank" rel="noopener noreferrer" className="book-link">Download</a>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+}
+
+export default Booklist;
