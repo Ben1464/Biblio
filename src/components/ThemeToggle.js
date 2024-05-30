06b6b725888f7c29100f9c57a0cdef88d9aeb340
Ben <kimaniben14@gmail.com>
2024-05-30 14:29:32 +0100

    made the darl/light icon functional

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,16 +1,22 @@
 // src/components/ThemeToggle.js
+import '../styles/ThemeToggle.css';
 import React, { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext'; // Adjust the path if necessary
 import '../styles/ThemeToggle.css'; // Ensure you create this CSS file for styles
-
+import lightModeIcon from '../assets/icons/brightness.png';
+import darkModeIcon from '../assets/icons/dark-mode.png';
 const ThemeToggle = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-
-  return (
-    <button onClick={toggleTheme} className={`theme-toggle ${theme}`}>
-      {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-    </button>
-  );
-};
-
-export default ThemeToggle;
+    const { theme, toggleTheme } = useContext(ThemeContext);
+  
+    return (
+      <button onClick={toggleTheme} className="theme-toggle">
+        <img
+          src={theme === 'light' ? darkModeIcon : lightModeIcon}
+          alt="Toggle theme"
+          className="theme-toggle-icon"
+        />
+      </button>
+    );
+  };
+  
+  export default ThemeToggle;
