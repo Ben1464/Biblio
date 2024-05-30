0b220d94acb32ec176c7eef87d9ac97df42a7115
Ben <kimaniben14@gmail.com>
2024-05-30 07:22:06 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,10 @@ function Header() {
         <h1 className="header-title">Bibliotech</h1>
         <nav className="header-nav">
           <ul className="nav-list">
-            <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
-            {/* <li className="nav-item"><a href="#books" className="nav-link">Books</a></li> */}
+            {/* <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="#books" className="nav-link">Books</a></li>
             <li className="nav-item"><a href="#about" className="nav-link">About</a></li>
-            <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+            <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li> */}
           </ul>
         </nav>
       </div>
