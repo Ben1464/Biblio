29247b8b3449ce4b01163616640144e22af2a090
Ben <kimaniben14@gmail.com>
2024-08-26 16:46:46 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Header.css'; // Ensure this file exists for styles
 import ThemeToggle from './ThemeToggle'; // Import ThemeToggle
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
@@ -19,23 +13,18 @@ function Header() {
       <div className="header-container">
         <h1 className="header-title">Biblio</h1>
         <ThemeToggle /> {/* Theme toggle button */}
-        
-        <button className="hamburger-menu" onClick={toggleMenu}>
-          <span className="hamburger-icon"></span>
-        </button>
-
-        <nav className={`header-nav ${menuOpen ? 'open' : ''}`}>
+        <div className="auth-buttons">
+          <SignedOut>
+            <SignInButton className="sign-in-button" />
+          </SignedOut>
+          <SignedIn>
+            <UserButton className="user-button" />
+          </SignedIn>
+        </div>
+        <nav className="header-nav">
           <ul className="nav-list">
             <li className="nav-item">
               <button onClick={scrollToTop} className="nav-link back-to-top-button">Back to Top</button>
-            </li>
-            <li className="nav-item auth-item">
-              <SignedOut>
-                <SignInButton className="sign-in-button" />
-              </SignedOut>
-              <SignedIn>
-                <UserButton className="user-button" />
-              </SignedIn>
             </li>
           </ul>
         </nav>
